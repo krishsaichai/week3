4d7b6d66c7ec32baed79a693f0dcdde72d9330e0
krishsaichai <22h51a05p5@cmrcet.ac.in>
2024-11-25 10:30:55 +0530

    Update prime.java

--- prime.java
+++ prime.java
@@ -1,19 +1,14 @@
-public  class prime {
-    public static void main(String[] args) {
-        int num = 100;
-        boolean isPrime = true;
-
-        for (int i = 2; i <= num / 2; i++) {
-            if (num % i == 0) {
-                isPrime = false;
-                break;
-            }
+class PrimeCheck {
+    static boolean isPrime(int n) {
+        if (n <= 1) return false;
+        for (int i = 2; i < n; i++) {
+            if (n % i == 0) return false;
         }
+        return true;
+    }
 
-        if (isPrime) {
-            System.out.println(num + " is a prime number.");
-        } else {
-            System.out.println(num + " is not a prime number.");
-        }
-    }
+    public static void main(String[] args) {
+        System.out.println(isPrime(19)); // true
+        System.out.println(isPrime(15)); // false
+    }
 }
